1601e5dc6e60fb59875ea646fc5491f91279c698
ladesiii <dromerol2122@politecnics.barcelona>
2024-12-04 16:58:37 +0100

    cambio conexion bd

--- php/bd.php
+++ php/bd.php
@@ -1,16 +1,18 @@
 <?php
-function openDB(){
+function openDB()
+{
 
-$servername = "sql207.byethost13.com";
-$username = "b13_37391685";
-$password = "desi123";
+    $servername = "hostingmysql335.nominalia.com";
+    $username = "daw01";
+    $pass = "Politecnics2024";
 
-
-$conexion = new PDO("mysql:host=$servername;dbname=b13_37391685_arcade", $username, $password);
-$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$conexion->exec("set names utf8");
-
-return $conexion;
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=daw01", $username, $pass);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $connection;
+    } catch (PDOException $e) {
+        die('Error: ' . $e->getMessage());
+    }
 }
 
 function closeDB()
@@ -20,47 +22,44 @@ function closeDB()
 
 function errorsMessage($e)
 {
-    if (!empty($e->errorInfo[1]))
-        {
-            switch ($e->errorInfo[1])
-            {
-                case 1062:
-                    $mensaje = 'registro duplicado';
-                    break;
-                case 1063:
-                    $mensaje = 'Registro con elementos relacionados';
-                    break;
-                default:
-                    $mensaje = $e->errorInfo[1] . ' - ' . $e->errorInfo[2];
-                    break;
-            }
+    if (!empty($e->errorInfo[1])) {
+        switch ($e->errorInfo[1]) {
+            case 1062:
+                $mensaje = 'registro duplicado';
+                break;
+            case 1063:
+                $mensaje = 'Registro con elementos relacionados';
+                break;
+            default:
+                $mensaje = $e->errorInfo[1] . ' - ' . $e->errorInfo[2];
+                break;
         }
-        else{
-            switch ($e->getCode()) 
-            {
-                case 1044:
-                    $mensaje = "Usuario y/o contraseña incorrectos";
-                    break;
-                case 1049:
-                    $mensaje = "Base de datos desconocida";
-                    break;
-                case 2002:
-                    $mensaje = 'No se encuentra el servidor';
-                    break;
-                default:
-                    $mensaje = $e->getCode() . ' - ' . $e->getMessage();
-                    break;
-            }
+    } else {
+        switch ($e->getCode()) {
+            case 1044:
+                $mensaje = "Usuario y/o contraseña incorrectos";
+                break;
+            case 1049:
+                $mensaje = "Base de datos desconocida";
+                break;
+            case 2002:
+                $mensaje = 'No se encuentra el servidor';
+                break;
+            default:
+                $mensaje = $e->getCode() . ' - ' . $e->getMessage();
+                break;
         }
-        return $mensaje;
+    }
+    return $mensaje;
 }
 
-function getUsuario(){
-    
+function getUsuario()
+{
+
     $conexion = openDB();
     $sentenciaText = "SELECT idUsuario, nombre, contrasenya, rol_idRol, tipo FROM usuario INNER JOIN rol ON rol.idRol = usuario.rol_idRol";
 
-    $stmt = $conexion ->prepare($sentenciaText);
+    $stmt = $conexion->prepare($sentenciaText);
     $stmt->execute();
 
     $group = $stmt->fetchAll();
@@ -69,130 +68,140 @@ function getUsuario(){
     return $group;
 }
 
-function getRoles() {
-    try {
-    $conexion = openDB();
-    $sentenciaText = "SELECT idRol, tipo FROM rol";
-
-    $stmt = $conexion->prepare($sentenciaText);
-    $stmt->execute();
-
-    $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    $conexion = closeDB();
-    return $roles;
-    } catch (PDOException $e) {
-        return errorsMessage($e);
-    }
-}
-
-function getUsuarioPorId($id) {
+function getRoles()
+{
     try {
         $conexion = openDB();
-    $sentenciaText = "SELECT idUsuario, nombre, contrasenya, rol_idRol FROM usuario WHERE idUsuario = :id";
+        $sentenciaText = "SELECT idRol, tipo FROM rol";
 
-    $stmt = $conexion->prepare($sentenciaText);
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
+        $stmt = $conexion->prepare($sentenciaText);
+        $stmt->execute();
 
-    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+        $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $conexion = closeDB();
-    return $usuario;
+        $conexion = closeDB();
+        return $roles;
     } catch (PDOException $e) {
         return errorsMessage($e);
     }
 }
 
-function getUsuarioByTipo($userId){
+function getUsuarioPorId($id)
+{
     try {
         $conexion = openDB();
-    $sentenciaText = 'SELECT * FROM usuario WHERE id = :idTipo';
-    $stmt = $conexion->prepare($sentenciaText);
-    $stmt->bindParam(':idTipo', $userId, PDO::PARAM_INT);
-    $stmt->execute();
+        $sentenciaText = "SELECT idUsuario, nombre, contrasenya, rol_idRol FROM usuario WHERE idUsuario = :id";
 
-    $group = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $conexion->prepare($sentenciaText);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
 
-    $conexion = closeDB();
-    return $group;
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $conexion = closeDB();
+        return $usuario;
     } catch (PDOException $e) {
         return errorsMessage($e);
     }
 }
 
-function registro($nombre, $contrasenya) {
+function getUsuarioByTipo($userId)
+{
+    try {
+        $conexion = openDB();
+        $sentenciaText = 'SELECT * FROM usuario WHERE id = :idTipo';
+        $stmt = $conexion->prepare($sentenciaText);
+        $stmt->bindParam(':idTipo', $userId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $group = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $conexion = closeDB();
+        return $group;
+    } catch (PDOException $e) {
+        return errorsMessage($e);
+    }
+}
+
+function registro($nombre, $contrasenya)
+{
     try {
         $conexion = openDB();
 
-    $stmt = $conexion->prepare("SELECT * FROM usuario WHERE nombre = :nombre");
-    $stmt->bindParam(':nombre', $nombre);
-    $stmt->execute();
+        $stmt = $conexion->prepare("SELECT * FROM usuario WHERE nombre = :nombre");
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->execute();
 
-    if ($stmt->rowCount() > 0) {
-        return false;
-    }
+        if ($stmt->rowCount() > 0) {
+            return false;
+        }
 
-    $sentenciaText = "INSERT INTO usuario (nombre, contrasenya, rol_idRol) VALUES (:nombre, :contrasenya, 1)";
-    $stmt = $conexion->prepare($sentenciaText);
-    $stmt->bindParam(':nombre', $nombre);
-    $stmt->bindParam(':contrasenya', $contrasenya);
+        $sentenciaText = "INSERT INTO usuario (nombre, contrasenya, rol_idRol) VALUES (:nombre, :contrasenya, 1)";
+        $stmt = $conexion->prepare($sentenciaText);
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':contrasenya', $contrasenya);
 
-    $stmt->execute();
+        $stmt->execute();
+        if (!$stmt->execute()) {
+            print_r($stmt->errorInfo());
+        }
 
-    $conexion = closeDB();
+        $conexion = closeDB();
 
-    return true;
+        return true;
     } catch (PDOException $e) {
         return errorsMessage($e);
     }
 }
 
-function modificarUsuario($idUsuario, $nombre, $contrasenya, $rol_idRol) {
+function modificarUsuario($idUsuario, $nombre, $contrasenya, $rol_idRol)
+{
 
     try {
         $conexion = openDB();
-    $sentenciaText = "UPDATE usuario SET nombre = :nombre, contrasenya = :contrasenya, rol_idRol = :rol_idRol WHERE idUsuario = :idUsuario";
+        $sentenciaText = "UPDATE usuario SET nombre = :nombre, contrasenya = :contrasenya, rol_idRol = :rol_idRol WHERE idUsuario = :idUsuario";
 
-    $stmt = $conexion->prepare($sentenciaText);
-    $stmt->bindParam(':nombre', $nombre);
-    $stmt->bindParam(':contrasenya', $contrasenya);
-    $stmt->bindParam(':rol_idRol', $rol_idRol);
-    $stmt->bindParam(':idUsuario', $idUsuario);
+        $stmt = $conexion->prepare($sentenciaText);
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':contrasenya', $contrasenya);
+        $stmt->bindParam(':rol_idRol', $rol_idRol);
+        $stmt->bindParam(':idUsuario', $idUsuario);
 
-    $stmt->execute();
+        $stmt->execute();
 
-    closeDB();
+        closeDB();
     } catch (PDOException $e) {
         return errorsMessage($e);
     }
 }
 
 
-function borrarUsuario($idUsuario){
+function borrarUsuario($idUsuario)
+{
 
     try {
         $conexion = openDB();
         $sentenciaText = "DELETE FROM usuario WHERE idUsuario = :idUsuario";
-    
+
         $stmt = $conexion->prepare($sentenciaText);
         $stmt->bindParam(':idUsuario', $idUsuario);
         $stmt->execute();
-    
+
         $conexion = closeDB();
     } catch (PDOException $e) {
         return errorsMessage($e);
     }
 
-   
-    
+
+
 }
 
-function getRanking() {
+function getRanking()
+{
     try {
         $conexion = openDB();
 
-        $sentenciaText ='SELECT r.usuario_idUsuario, u.nombre AS usuario_nombre, r.juegos_idJuego, j.titulo AS juego_titulo, r.puntuacion
+        $sentenciaText = 'SELECT r.usuario_idUsuario, u.nombre AS usuario_nombre, r.juegos_idJuego, j.titulo AS juego_titulo, r.puntuacion
             FROM ranking r INNER JOIN  usuario u ON r.usuario_idUsuario = u.idUsuario 
             INNER JOIN juegos j ON r.juegos_idJuego = j.idJuego ORDER BY r.puntuacion DESC';
 
