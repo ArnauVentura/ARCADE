7d2d6cb4591d524d6a85a0558d9eacc98b17feee
Desirée Romero <dromerol2122@politecnics.barcelona>
2024-12-05 12:14:47 +0100

    cambio titulos de los archivos

--- html/fuentes.php
+++ html/fuentes.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anna y el misterio de las 4 fuentes</title>
+    <title>Anna y el misterio de las 3 fuentes</title>
     <link rel="stylesheet" href="../css/Style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
