7cdc246340cc70d48c38e1b86d42cf58d992df88
Arnau <aventurac2122@politecnics.barcelona>
2024-12-16 15:28:54 +0100

    apis

---
+++ php/getRanking.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+require_once('./bd.php');
+
+function guardarRanking($usuario_idUsuario, $juegos_idJuego, $puntuacion) {
+    try {
+        $conexion = openDB();
+
+        $sentenciaText = "INSERT INTO ranking (usuario_idUsuario, juegos_idJuego, puntuacion) VALUES (:usuario_idUsuario, :juegos_idJuego, :puntuacion)";
+        $stmt = $conexion->prepare($sentenciaText);
+
+        $stmt->bindParam(':usuario_idUsuario', $usuario_idUsuario, PDO::PARAM_INT);
+        $stmt->bindParam(':juegos_idJuego', $juegos_idJuego, PDO::PARAM_INT);
+        $stmt->bindParam(':puntuacion', $puntuacion, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        closeDB();
+
+        return true; 
+    } catch (PDOException $e) {
+        echo "Error al guardar el ranking: " . $e->getMessage();
+        return false;
+    }
+}
+
+?>
